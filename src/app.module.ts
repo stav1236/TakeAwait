7f03099909662b54c5d74a6e0e0e48fe407a6ae8
unknown <stavmaor1@gmail.com>
2024-02-28 15:18:51 +0200

    use nest config for db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,27 @@
 import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose";
 
-import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-
+import { AppController } from "./app.controller";
 import { DishModule } from "./modules/Dish/dish.module";
 import { RestaurantModule } from "./modules/Restaurant/restaurant.module";
 
+import config from "src/common/config/configuration";
+
 @Module({
   imports: [
-    MongooseModule.forRoot("mongodb://localhost:27017/TakeAwait"),
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [config],
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>("database.url"),
+      }),
+      inject: [ConfigService],
+    }),
     DishModule,
     RestaurantModule,
   ],
---
+++ src/common/config/configuration.ts
@@ -0,0 +1,4 @@
+export default () => ({
+  port: parseInt(process.env.PORT) ?? 3000,
+  database: { url: process.env.DB_URL ?? "" },
+});
