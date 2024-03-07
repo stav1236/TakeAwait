4fcdf6771345dd47e40d33567c7f55d6015ebe3f
stav1236 <stavmaor1@gmail.com>
2024-03-07 14:28:03 +0200

    get all dishes

--- src/modules/Dish/dish.module.ts
+++ src/modules/Dish/dish.module.ts
@@ -2,8 +2,12 @@ import { Module } from "@nestjs/common";
 import { MongooseModule } from "@nestjs/mongoose";
 
 import { Dish, DishSchema } from "./dish.schema";
+import { DishService } from "./dish.service";
+import { DishController } from "./dish.controller";
 
 @Module({
+  providers: [DishService],
+  controllers: [DishController],
   imports: [
     MongooseModule.forFeature([{ name: Dish.name, schema: DishSchema }]),
   ],
---
+++ src/modules/Dish/dish.service.ts
@@ -0,0 +1,36 @@
+import { Model } from "mongoose";
+import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+
+import { Dish } from "./dish.schema";
+import { DishType } from "./dish.constants";
+
+@Injectable()
+export class DishService {
+  constructor(@InjectModel(Dish.name) private dishModel: Model<Dish>) {}
+
+  async getAllDishesGroupedByType(): Promise<{ [key in DishType]?: Dish[] }> {
+    const groupedDishes = await this.dishModel.aggregate([
+      {
+        $group: {
+          _id: "$type",
+          dishes: { $push: "$$ROOT" },
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          type: "$_id",
+          dishes: 1,
+        },
+      },
+    ]);
+
+    const result: { [key in DishType]?: Dish[] } = {};
+    groupedDishes.forEach((group) => {
+      result[group.type] = group.dishes;
+    });
+
+    return result;
+  }
+}
