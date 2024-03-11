68c6df9d12775671aeea06f2785f73b05bbc3f6a
yuly <211496120@idf.il>
2024-03-11 16:18:11 +0200

    calc daily report by using db

--- src/jobs/report/report.module.ts
+++ src/jobs/report/report.module.ts
@@ -2,9 +2,10 @@ import { Module } from "@nestjs/common";
 
 import { ReportService } from "./report.service";
 import { RestaurantModule } from "src/modules/Restaurant/restaurant.module";
+import { OrderModule } from "src/modules/Order/order.module";
 
 @Module({
-  imports: [RestaurantModule],
+  imports: [RestaurantModule, OrderModule],
   providers: [ReportService],
   exports: [ReportService],
 })
--- src/jobs/report/report.service.ts
+++ src/jobs/report/report.service.ts
@@ -1,52 +1,45 @@
 import * as fs from "fs";
-import { Model } from "mongoose";
 
-import { Cron, CronExpression } from "@nestjs/schedule";
 import { Injectable } from "@nestjs/common";
-import { InjectModel } from "@nestjs/mongoose";
+import { Cron, CronExpression } from "@nestjs/schedule";
 
-import { Restaurant } from "src/modules/Restaurant/restaurant.schema";
+import { OrderService } from "src/modules/Order/order.service";
 import { RestaurantService } from "src/modules/Restaurant/restaurant.service";
+import { getDateWithTimeString } from "src/common/utilities/dataUtils";
 
 @Injectable()
 export class ReportService {
-  constructor(private readonly restaurantService: RestaurantService) {}
+  constructor(
+    private readonly orderService: OrderService,
+    private readonly restaurantService: RestaurantService
+  ) {}
 
   @Cron(CronExpression.EVERY_MINUTE)
   async createFolder(): Promise<void> {
-    const timestamp = new Date()
-      .toLocaleString("he-IL", {
-        year: "numeric",
-        month: "2-digit",
-        day: "2-digit",
-        hour: "2-digit",
-        minute: "2-digit",
-        second: "2-digit",
-      })
-      .replace(/:/g, "-");
-
-    const reportFolderName = `assets/reports/${timestamp}`;
-
+    const curDate = new Date();
+    const timeString = getDateWithTimeString(curDate);
+    const reportFolderName = `assets/reports/${timeString}`;
     const restaurantsNames = await this.restaurantService.getAllRestaurantsNames();
 
     try {
-      // fs.mkdirSync(reportFolderName, { recursive: true });
+      fs.mkdirSync(reportFolderName, { recursive: true });
 
-      restaurantsNames.forEach((restaurant) => {
+      const promises = restaurantsNames.map(async (restaurant) => {
         try {
           const restaurantReportFileName = `${reportFolderName}/${restaurant.name}.json`;
-          const jsonData = JSON.stringify({
-            ordersAmount: 999,
-            avgCost: 999,
-            cancelsAmount: 999,
-          });
+          const orderData = await this.orderService.calcTodayOrdersReport(
+            restaurant._id.toString(),
+            curDate
+          );
 
-          // fs.writeFileSync(restaurantReportFileName, jsonData);
-          console.log(`file report "${restaurantReportFileName}" created successfully.`);
+          fs.writeFileSync(restaurantReportFileName, JSON.stringify(orderData));
+          console.log(`File report "${restaurantReportFileName}" created successfully.`);
         } catch (error) {
           console.error(`Error creating file: ${error}`);
         }
       });
+
+      await Promise.all(promises);
     } catch (error) {
       console.error(`Error creating folder: ${error}`);
     }
