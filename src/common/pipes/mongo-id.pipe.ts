99468d4567a5bec8620db1a5e2eed0b16545fcb4
yuly <211496120@idf.il>
2024-03-13 10:21:47 +0200

    check validation

---
+++ src/common/pipes/mongo-id.pipe.ts
@@ -0,0 +1,14 @@
+import { Types } from "mongoose";
+import { PipeTransform, Injectable, BadRequestException, ArgumentMetadata } from "@nestjs/common";
+
+@Injectable()
+export class MongoIdPipe implements PipeTransform<string, Types.ObjectId> {
+  transform(value: string, metadata: ArgumentMetadata): Types.ObjectId {
+    if (!Types.ObjectId.isValid(value)) {
+      const variableName = metadata.data;
+
+      throw new BadRequestException(`${variableName} is invalid ObjectId`);
+    }
+    return Types.ObjectId.createFromHexString(value);
+  }
+}
